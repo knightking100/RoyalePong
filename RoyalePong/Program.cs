1bf8c8893f4c12b95ae25a7adccc6ff571cc82c0
knightking100 <knightking123456789@gmail.com>
2017-04-01 19:21:51 +0700

    Add project files.

---
+++ RoyalePong/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RoyalePong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Letters);
+            new Common.Cryptography.xsalsa20poly1305();
+        }
+        const string Letters = "Happy April Fool";
+
+       
+    }
+}
